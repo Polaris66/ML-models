5065e1b387572d5a5911e39bb52f8bfa8c228750
Polaris66 <mathematician2b@gmail.com>
2023-03-04 16:07:20 +0530

    made py functions

--- classes/perceptron.py
+++ classes/perceptron.py
@@ -1,70 +1,29 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": 19,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "import pandas as pd\n",
-    "import numpy as np"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": 20,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "class Perceptron:\n",
-    "    def fit(self,X,y):\n",
-    "        #Initialization\n",
-    "        X[\"bias\"] = 1\n",
-    "        self.weight= np.zeros(X.shape[1])\n",
-    "\n",
-    "        while True:\n",
-    "            misclassifications = 0\n",
-    "            for i in range(X.shape[0]):\n",
-    "                X_i = X.iloc[i,:]\n",
-    "                y_i = y.iloc[i,:]\n",
-    "                if y_i*(np.dot(self.weight,X_i))<=0:\n",
-    "                    self.weight = self.weight+y_i*X_i\n",
-    "                    misclassifications+=1\n",
-    "            if misclassifications==0:\n",
-    "                break\n",
-    "\n",
-    "    def predict(self,X):\n",
-    "        prediction = np.empty()\n",
-    "        for i in range(X.shape[0]):\n",
-    "            X_i = X.iloc[i,:]\n",
-    "            if np.dot(X_i,self.weight)>0:\n",
-    "                prediction.append(1)\n",
-    "            else:\n",
-    "                prediction.append(-1)\n",
-    "        return prediction"
-   ]
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.10.6"
-  },
-  "orig_nbformat": 4
- },
- "nbformat": 4,
- "nbformat_minor": 2
-}
+import pandas as pd
+import numpy as np
+
+class Perceptron:
+    def fit(self,X,y):
+        #Initialization
+        X["bias"] = 1
+        self.weight= np.zeros(X.shape[1])
+
+        while True:
+            misclassifications = 0
+            for i in range(X.shape[0]):
+                X_i = X.iloc[i,:]
+                y_i = y.iloc[i,:]
+                if y_i*(np.dot(self.weight,X_i))<=0:
+                    self.weight = self.weight+y_i*X_i
+                    misclassifications+=1
+            if misclassifications==0:
+                break
+
+    def predict(self,X):
+        prediction = np.empty()
+        for i in range(X.shape[0]):
+            X_i = X.iloc[i,:]
+            if np.dot(X_i,self.weight)>0:
+                prediction.append(1)
+            else:
+                prediction.append(-1)
+        return prediction
--- classes/preprocessing.py
+++ classes/preprocessing.py
@@ -1,47 +1,20 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "import pandas as pd\n",
-    "import numpy as np"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "class Preprocessing:\n",
-    "    def fillna(self,df):\n",
-    "        for col in df.columns:\n",
-    "            if col==\"diagnosis\":\n",
-    "                df[col].fillna(value = df[col].mode(),inplace=True)\n",
-    "            elif col!=\"id\":\n",
-    "                df[col].fillna(value = df[col].mean(),inplace=True)\n",
-    "        return df\n",
-    "\n",
-    "    def normalize(self,df):\n",
-    "        df.iloc[:,3:] =  (df.iloc[:,3:] - df.iloc[:,3:].mean())/df.iloc[:,3:].std()\n",
-    "        return df\n",
-    "    \n",
-    "    def train_test_split(df,train_size = 0.67, test_size=0.33,random_state=0):\n",
-    "        train = df.sample(frac= test_size,random_state = random_state)\n",
-    "        test = df.drop(train.index)\n",
-    "        return train, test"
-   ]
-  }
- ],
- "metadata": {
-  "language_info": {
-   "name": "python"
-  },
-  "orig_nbformat": 4
- },
- "nbformat": 4,
- "nbformat_minor": 2
-}
+import pandas as pd
+import numpy as np
+
+class Preprocessing:
+    def fillna(self,df):
+        for col in df.columns:
+            if col=="diagnosis":
+                df[col].fillna(value = df[col].mode(),inplace=True)
+            elif col!="id":
+                df[col].fillna(value = df[col].mean(),inplace=True)
+        return df
+
+    def normalize(self,df):
+        df.iloc[:,3:] =  (df.iloc[:,3:] - df.iloc[:,3:].mean())/df.iloc[:,3:].std()
+        return df
+    
+    def train_test_split(df,train_size = 0.67, test_size=0.33,random_state=0):
+        train = df.sample(frac= test_size,random_state = random_state)
+        test = df.drop(train.index)
+        return train, test
